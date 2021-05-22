5f2611210f7cede658ff4d0d7dcb27b4bc1dafb2
Sarang Narkhede <sn7330@rit.edu>
2021-05-22 16:12:29 -0400

    185. Department Top Three Salaries

---
+++ 001_DepartmentTopThreeSalaries.sql
@@ -0,0 +1,8 @@
+select d.Name Department, e1.Name Employee, e1.Salary
+from Employee e1, Department d
+where e1.DepartmentId = d.Id
+and 3 > (select count(distinct(e2.Salary)) 
+                  from Employee e2 
+                  where e2.Salary > e1.Salary 
+                  and e1.DepartmentId = e2.DepartmentId
+                  ) 
