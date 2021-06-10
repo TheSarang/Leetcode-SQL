bc63b2e689a5549a586059829de1a3326126bb4b
Sarang Narkhede <sn7330@rit.edu>
2021-06-10 15:42:05 -0400

    1412. Find the Quiet Students in All Exams

--- 003_FindtheQuietStudentsinAllExams.sql
+++ 003_FindtheQuietStudentsinAllExams.sql
@@ -1,3 +1,17 @@
+SELECT student_id, student_name 
+FROM Student
+WHERE student_id IN 
+    (SELECT DISTINCT student_id FROM Exam)
+  AND student_id NOT IN 
+      (
+        SELECT student_id
+        FROM Exam e1
+        WHERE score = (SELECT max(score) FROM Exam e2 WHERE e1.exam_id = e2.exam_id)
+          OR score = (SELECT min(score) FROM Exam e2 WHERE e1.exam_id = e2.exam_id)
+      )
+      
+--  ---------------------------------------------------------------------------
+
 select distinct s.student_id, s.student_name
 from Student s, Exam e
 where s.student_id = e.student_id
